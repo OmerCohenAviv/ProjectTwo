8c0bbea29065b5949ef0c353dbb3111a95c6c333
Pokernab <53443894+Pokernab@users.noreply.github.com>
2019-09-20 07:29:59 +0300

    fixed login

--- src/components/Answers/AnswerUI/AnswerUI.js
+++ src/components/Answers/AnswerUI/AnswerUI.js
@@ -4,16 +4,16 @@ import { Card, Button } from 'react-bootstrap';
 import {NavLink} from 'react-router-dom';
 
 const answerUI = (props) => {
-    const { userName, question } = props
+    const { userName, el } = props
     return (
         <Card style={{ width: '18rem' }}>
             <Card.Img variant="top" src={props.avatar} />
             <Card.Body>
                 <Card.Title> Asked By - {userName} </Card.Title>
                 <Card.Text>
-                    optionOne - {question.optionOne.text}
+                    optionOne - {el.optionOne.text}
                 </Card.Text>
-                <NavLink to={`/questions/${question.id}`}>
+                <NavLink to={`/questions/${el.id}`}>
                     <Button variant="primary" >Watch Full answer INFO</Button>
                 </NavLink>
             </Card.Body>
